6da47ad3d78433d6ec6cbb1bff4b269182373790
javiergarcia.jgc <javiergarcia.jgc@gmail.com>
2018-02-27 18:36:15 -0500

    Added check for multiple masters

--- Services/masters.js
+++ Services/masters.js
@@ -1,8 +1,11 @@
-let master = require('../private/master.json');
+let masters = require('../private/masters.json');
 
-exports.isMaster= function(id) {
-  if (id ==  master.id)
-    return true;
-
+exports.isMaster = function(id) {
+  for (let value of masters) {
+    if (id == value)
+      return true;
+  }
+  console.log('fail')
   return false;
+
 }
