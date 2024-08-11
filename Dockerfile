828134446c1d903eb6775a72662228160bb7010c
Habier <Habier@users.noreply.github.com>
2024-08-12 01:41:54 +0200

    Upgraded dependencies and moved to docker

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM node:lts-alpine
+
+WORKDIR /usr/app
+
+# use changes to package.json to force Docker not to use the cache
+# when we change our application's nodejs dependencies:
+COPY package*.json ./
+
+COPY . .
+
+RUN npm install
+
+CMD ["npm", "start"]
--- bot.js
+++ bot.js
@@ -1,41 +1,41 @@
 require('./config');
-const { Client, Intents } = require('discord.js');
+const {Client, Intents, GatewayIntentBits} = require('discord.js');
 var auth = require('./private/auth.json');
 var special = require('./special');
 var Commands = require('./commands');
 
 // Initialize Discord Bot
-global.bot = new Client({ intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES"] });
+global.bot = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.DirectMessages]});
 
 bot.once('ready', () => {
-	logger.info('Connected');
-	logger.info('Logged in as: ' + bot.user.username + ' - (' + bot.user.id + ')');
-	bot.user.setActivity(lang.bot_activity);
+    logger.info('Connected');
+    logger.info('Logged in as: ' + bot.user.username + ' - (' + bot.user.id + ')');
+    bot.user.setActivity(lang.bot_activity);
 });
 
 bot.on('messageCreate', (message) => {
-	if (message.author.bot)
-		return; //lets ignore messages from bots.
+    if (message.author.bot)
+        return; //lets ignore messages from bots.
 
-	if (message.content[0] == conf.character) {
-		var args = message.content.substring(1).split(' ');
-		let success = Commands.execute(args, message);
+    if (message.content[0] == conf.character) {
+        var args = message.content.substring(1).split(' ');
+        let success = Commands.execute(args, message);
 
-		if (success)
-			message.react("👍");
+        if (success)
+            message.react("👍");
 
-		special.comms(args, message);
-	}
+        special.comms(args, message);
+    }
 
 });
 
 if (conf.enable_greeting)
-	bot.on('guildMemberAdd', member => {
-		// Send the message to the channel specified in _configurer.js
-		let channel = member.guild.channels.find('name', conf.defaultChannel);
-		if (!channel)
-			return; //channel not found, better get out
-		channel.send(lang.use(lang.join_welcome, member, conf.character + 'help'));
-	});
+    bot.on('guildMemberAdd', member => {
+        // Send the message to the channel specified in _configurer.js
+        let channel = member.guild.channels.find('name', conf.defaultChannel);
+        if (!channel)
+            return; //channel not found, better get out
+        channel.send(lang.use(lang.join_welcome, member, conf.character + 'help'));
+    });
 
 bot.login(auth.token);
